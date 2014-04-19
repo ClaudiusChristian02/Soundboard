b2f5570c500be0f1494e477560a66e19a306a534
Claudius Christiain <Claudius Christiain>
2014-04-19 16:40:25 -0400

    complete

---
+++ src/com/example/soundboardtt/Burgundy.java
@@ -0,0 +1,16 @@
+package com.example.soundboardtt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Burgundy extends Activity{
+
+	@Override
+	protected void onCreate(Bundle practice) {
+		// TODO Auto-generated method stub
+		super.onCreate(practice);
+	    setContentView(R.layout.burgundy);
+	}
+	
+
+}
